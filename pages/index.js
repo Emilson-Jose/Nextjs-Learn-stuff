41803c2f7774ca0c3a6d1ea9d51ea2b7a2ba3733
Emilson-Jose <emilson.h.jose@gmail.com>
2024-02-29 20:23:25 -0500

    create folio and test in index.js
    also changed site colors

--- pages/index.js
+++ pages/index.js
@@ -41,6 +41,20 @@ export default function Home({ allPostsData }) {
           ))}
         </ul>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Projects, Code</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
 }
